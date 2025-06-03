44b7c1b16abca411ab2a3ef5e4eaf78f281928e1
Bryan Hann <nobody@nowhere>
2025-06-03 14:41:07 -0230

    Move all [.bch] code under [./.bch]

--- .bch/activate.sh
+++ .bch/activate.sh
@@ -4,16 +4,16 @@ __root__=$(dirname $(dirname ${BASH_SOURCE[0]}))
 __export__() { export BCH_PROMPT_${1}="${2}" ; }
 
 __export__  _root  ${__root__}
-__export__  _bin   ${__root__}/.bch.bin
-__export__  _lbin  ${__root__}/.bch.lbin
-__export__  _init  ${__root__}/.bch.init/init.sh
-__export__  _vendor ${__root__}/.bch.vendor
+__export__  _bin   ${__root__}/.bch/bin
+__export__  _lbin  ${__root__}/.bch/lbin
+__export__  _init  ${__root__}/.bch/init/init.sh
+__export__  _vendor ${__root__}/.bch/vendor
 __export__  taskfile ~/.tmp/bch.prompt.taskfile
 
-bch:000:linkall ${__root__}/.bch.lbin
-bch:000:append  ${__root__}/.bch.bin
+bch:000:linkall ${__root__}/.bch/lbin
+#bch:000:append  ${__root__}/.bch/bin
 
-source ${__root__}/.bch.init/init.sh
+source ${__root__}/.bch/init/init.sh
 
 unset __root__
 unset __export__
